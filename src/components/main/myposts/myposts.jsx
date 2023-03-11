faea68764a11faea719d55309413922c9d1f1b01
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-03-11 16:09:34 +0200

    11/03 add style profile

--- src/components/main/myposts/myposts.jsx
+++ src/components/main/myposts/myposts.jsx
@@ -9,9 +9,11 @@ const MyPosts = ({ state }) => {
   });
   return (
     <div className={style["app-main__posts"]}>
-      <textarea cols="30" rows="10"></textarea>
-      <button>add</button>
-      <button>remove</button>
+      <div className={style["app-main__posts-create"]}>
+          <textarea cols="30" rows="10"></textarea>
+          <button className={style["app-main__posts-add"]}>add</button>
+          <button className={style["app-main__posts-remove"]}>remove</button>
+      </div>
       <div className={style["main__posts-post"]}>{postsUi}</div>
     </div>
   );
