99e5118a1cbbd491ef856de85460ab49fb52abfc
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-03-13 09:20:52 +0200

    13/03 change state on store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import state, { addPost, subscribe, updateMessage } from './redux/state';
+
+import state, { addPost, store, subscribe, updateMessage } from './redux/state';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
@@ -8,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const renderDomTree = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateMessage={updateMessage} />
+      <App state={store.getState()} addPost={addPost} updateMessage={updateMessage} />
     </React.StrictMode>
   );
 }
--- src/redux/state.js
+++ src/redux/state.js
@@ -2,11 +2,82 @@ import img1 from "../assets/ava1.jpeg"
 import img2 from "../assets/ava2.jpeg"
 import img3 from "../assets/ava3.jpeg"
 import { nanoid } from 'nanoid'
-
 function id() {
     return nanoid();
 }
 
+export let store = {
+    _state:{
+        profile: {
+            name: "Anton",
+            surname: "Vasylchenko",
+            city: "Konotop",
+            age: 26
+        },
+        nav: {
+            sideBar: [
+                { id: id(), link: "Profie", path: "/profile" },
+                { id: id(), link: "Message", path: "/dialogs" },
+                { id: id(), link: "News", path: "/news" },
+                { id: id(), link: "Music", path: "/music" },
+                { id: id(), link: "Settings", path: "/settings" },
+                { id: id(), link: "Friends", path: "/friends" }
+            ],
+            friends: [
+                { id: id(), name: "Anton", isActive: false, img: img1 },
+                { id: id(), name: "Oksana", isActive: true, img: img2 },
+                { id: id(), name: "Dima", isActive: false, img: img3 }
+            ]
+        },
+        dataPosts: {
+            post: [
+                { id: id(), message: "Hello world", like: 25 },
+                { id: id(), message: "First post", like: 15 },
+            ]
+        },
+        dataDialogs: {
+            dialogs: [
+                { id: id(), name: "Anton" },
+                { id: id(), name: "Oksana" },
+                { id: id(), name: "Sasha" },
+                { id: id(), name: "Dima" },
+            ],
+            messages: [
+                { id: id(), messege: "Hello" },
+                { id: id(), messege: "World" },
+                { id: id(), messege: "My" },
+                { id: id(), messege: "Name" },
+            ]
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    renderDomTree() {
+        console.log("render true");
+    },
+    addPost(postBody){
+        let newPost = {
+            id: id(),
+            message: postBody,
+            like: 0
+        }
+        state.dataPosts.post.push(newPost);
+        renderDomTree(state) 
+    },
+    updateMessage(bodyMessage){
+        let newMessage = {
+            id: id(),
+            messege: bodyMessage
+        }
+        state.dataDialogs.messages.push(newMessage);
+        renderDomTree(state) 
+    },
+    subscribe(observer){
+        renderDomTree = observer
+    }
+    
+}
 
 function renderDomTree() {
     // Render DOM(34-35)
